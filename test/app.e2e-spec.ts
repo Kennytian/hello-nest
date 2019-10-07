293900407568f9cd52b5c99a27f6c30129bb9831
Kennytian <kt.tian@gmail.com>
2019-10-07 21:01:44 +0800

    Add cats sample file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app/app.module';
+import { CatsModule } from '../src/cats/module';
 
 describe('AppController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, CatsModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -19,5 +20,26 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/cats/find-all (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/cats/find-all')
+      .expect(200)
+      .expect('This action result all cats');
+  });
+
+  it('/cats/create (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/cats/create')
+      .expect(201)
+      .expect('This action adds a new cat');
+  });
+
+  it('/cats/find-wildcard (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/cats/find-wildcard')
+      .expect(201)
+      .expect('This action result a wildcard');
   });
 });
